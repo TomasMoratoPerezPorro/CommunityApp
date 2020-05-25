34aea3cb70d92a326aac4dd4e07a33ff928538db
TomasMoratoPerezPorro <49519581+TomasMoratoPerezPorro@users.noreply.github.com>
2020-05-25 19:52:01 +0200

    Programacio ChangeNotifierProvider NewReserva

--- appcomunity/lib/Pages/NewReservaPage.dart
+++ appcomunity/lib/Pages/NewReservaPage.dart
@@ -1,97 +1,208 @@
+import 'package:appcomunity/Model/Espais.dart';
 import 'package:appcomunity/Widgets/LlistaEspaisWidget.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:provider/provider.dart';
 
-class NewReservaPage extends StatefulWidget {
-  @override
-  _NewReservaPageState createState() => _NewReservaPageState();
+class NewReservaProvider with ChangeNotifier {
+  DateTime pickedDate = DateTime.now();
+  TimeOfDay time = TimeOfDay.now();
+  Espais selectedEspai;
+  int selectedDuracio = 1;
+
+  void setDuracio(int duracio){
+    this.selectedDuracio = duracio;
+    notifyListeners();
+  }
+ 
+
+  void setDate(DateTime date) {
+    this.pickedDate = date;
+    notifyListeners();
+  }
+
+  void setTime(TimeOfDay hora) {
+    this.time = hora;
+    notifyListeners();
+  }
+
+  void setEspai(Espais espai){
+    this.selectedEspai = espai;
+    notifyListeners();
+  }
 }
 
-class _NewReservaPageState extends State<NewReservaPage> {
-  TextEditingController _controller;
-  DateTime pickedDate;
-  TimeOfDay time;
-
-  @override
-  void initState() {
-    _controller = TextEditingController();
-    pickedDate = DateTime.now();
-    time = TimeOfDay.now();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
+class NewReservaPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text('New Reserva')),
-        body: SingleChildScrollView(
+        body: FormNewReservaWidget());
+  }
+}
+
+class FormNewReservaWidget extends StatelessWidget {
+  const FormNewReservaWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<NewReservaProvider>(
+        create: (context) => NewReservaProvider(),
+        child: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              LlistaEspaisWidget(),
-              Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Column(
-                    children: <Widget>[
-                      Text("Selecciona el dia:"),
-                      ListTile(
-                        title: Text(
-                            "Date: ${pickedDate.year}, ${pickedDate.month}, ${pickedDate.day}"),
-                        trailing: Icon(Icons.keyboard_arrow_down),
-                        onTap: _pickDate,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Column(
-                    children: <Widget>[
-                      Text("Selecciona la hora de inici:"),
-                      ListTile(
-                        title: Text("Date: ${time.hour}, ${time.minute}"),
-                        trailing: Icon(Icons.keyboard_arrow_down),
-                        onTap: _pickTime,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+              FieldEspaiWidget(),
+              FieldDiaWidget(),
+              FieldHoraWidget(),
+              FieldDuracioWidget()
             ],
           ),
         ));
   }
+}
 
-  _pickDate() async {
-    DateTime date = await showDatePicker(
-      context: context,
-      firstDate: DateTime(DateTime.now().year - 5),
-      lastDate: DateTime(DateTime.now().year + 5),
-      initialDate: pickedDate,
+class FieldDuracioWidget extends StatelessWidget {
+  const FieldDuracioWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final myProvider = Provider.of<NewReservaProvider>(context, listen: true);
+    Color getColor(int i){
+       if(myProvider.selectedDuracio==i){
+         return Colors.red;
+       }else{
+         return Colors.grey[350];
+       }
+     } 
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.all(10),
+              child: Text("Selecciona la hora de inici:"),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                for (var i = 1; i < 5; i++)
+                  RaisedButton(
+                    color: getColor(i),
+                    onPressed: () {
+                      myProvider.setDuracio(i);
+                    },
+                    child: Text("${i} Hora"),
+                  )
+              ],
+            ),
+          ],
+        ),
+      ),
     );
-
-    if (date != null)
-      setState(() {
-        pickedDate = date;
-      });
   }
+}
 
-  _pickTime() async {
-    TimeOfDay t = await showTimePicker(context: context, initialTime: time);
+class FieldHoraWidget extends StatelessWidget {
+  const FieldHoraWidget({
+    Key key,
+  }) : super(key: key);
 
-    if (t != null)
-      setState(() {
-        time = t;
-      });
+  @override
+  Widget build(BuildContext context) {
+    final myProvider = Provider.of<NewReservaProvider>(context, listen: true);
+    _pickTime() async {
+      TimeOfDay t =
+          await showTimePicker(context: context, initialTime: myProvider.time);
+
+      if (t != null) myProvider.setTime(t);
+    }
+
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text("Selecciona la hora de inici:"),
+            ListTile(
+              title: Text(
+                  "Date: ${myProvider.time.hour}, ${myProvider.time.minute}"),
+              trailing: Icon(Icons.keyboard_arrow_down),
+              onTap: _pickTime,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class FieldDiaWidget extends StatelessWidget {
+  const FieldDiaWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final myProvider = Provider.of<NewReservaProvider>(context, listen: true);
+    _pickDate() async {
+      DateTime date = await showDatePicker(
+        context: context,
+        firstDate: DateTime(DateTime.now().year - 5),
+        lastDate: DateTime(DateTime.now().year + 5),
+        initialDate: myProvider.pickedDate,
+      );
+
+      if (date != null) myProvider.setDate(date);
+    }
+
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text("Selecciona el dia:"),
+            ListTile(
+              title: Text(
+                  "Date: ${myProvider.pickedDate.year}, ${myProvider.pickedDate.month}, ${myProvider.pickedDate.day}"),
+              trailing: Icon(Icons.keyboard_arrow_down),
+              onTap: _pickDate,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class FieldEspaiWidget extends StatelessWidget {
+  const FieldEspaiWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: Padding(
+      padding: const EdgeInsets.all(10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            "Selecciona el espai:",
+            textAlign: TextAlign.left,
+          ),
+          LlistaEspaisWidget(),
+        ],
+      ),
+    ));
   }
 }
--- appcomunity/lib/Widgets/LlistaEspaisWidget.dart
+++ appcomunity/lib/Widgets/LlistaEspaisWidget.dart
@@ -1,5 +1,7 @@
+import 'package:appcomunity/Pages/NewReservaPage.dart';
 import 'package:flutter/material.dart';
 import 'package:appcomunity/Model/Espais.dart';
+import 'package:provider/provider.dart';
 
 class LlistaEspaisWidget extends StatelessWidget {
   const LlistaEspaisWidget({
@@ -9,38 +11,35 @@ class LlistaEspaisWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<List<Espais>>(
-                  stream: espaisSnapshots(),
-                  builder: (BuildContext context,
-                      AsyncSnapshot<List<Espais>> snapshot) {
-                    if (snapshot.hasError) {
-                      return Center(
-                          child: Text('ERROR: ${snapshot.error.toString()}'));
-                    }
-                    switch (snapshot.connectionState) {
-                      case ConnectionState.waiting:
-                        // Estem esperant el primer valor
-                        return Center(child: Text("Waiting..."));
-                      case ConnectionState.active:
-                        List<Espais> espais = snapshot.data;
-                        return Column(
-                          children: <Widget>[
-                            for (int i = 0; i < espais.length; i++)
-                              PastillaEspai(espai: espais[i]),
-                          ],
-                        );
+      stream: espaisSnapshots(),
+      builder: (BuildContext context, AsyncSnapshot<List<Espais>> snapshot) {
+        if (snapshot.hasError) {
+          return Center(child: Text('ERROR: ${snapshot.error.toString()}'));
+        }
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+            // Estem esperant el primer valor
+            return Center(child: Text("Waiting..."));
+          case ConnectionState.active:
+            List<Espais> espais = snapshot.data;
+            return Column(
+              children: <Widget>[
+                for (int i = 0; i < espais.length; i++)
+                  PastillaEspai(espai: espais[i]),
+              ],
+            );
 
-                      // return Text("${snapshot.data.length}");
-                      case ConnectionState.done:
-                        return Center(child: Text("Done!"));
-                      case ConnectionState.none:
-                      default:
-                        return Placeholder();
-                    }
-                  },
-                );
+          // return Text("${snapshot.data.length}");
+          case ConnectionState.done:
+            return Center(child: Text("Done!"));
+          case ConnectionState.none:
+          default:
+            return Placeholder();
+        }
+      },
+    );
   }
 }
-
 
 class PastillaEspai extends StatelessWidget {
   const PastillaEspai({
@@ -51,33 +50,46 @@ class PastillaEspai extends StatelessWidget {
   final Espais espai;
   @override
   Widget build(BuildContext context) {
+    final myProvider = Provider.of<NewReservaProvider>(context, listen: true);
+     Color getColor(){
+       if(myProvider.selectedEspai==this.espai){
+         return Colors.red;
+       }else{
+         return Colors.grey[350];
+       }
+     } 
     return Padding(
       padding: const EdgeInsets.all(10),
-      child: Card(
-        color: Colors.grey[100],
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.all(10),
-              height: 50,
-              width: 50,
-              decoration:
-                  BoxDecoration(color: Colors.green, shape: BoxShape.circle),
-              child: Icon(Icons.home),
-            ),
-            Text(espai.nom),
-            Container(
-              margin: EdgeInsets.only(left: 200),
-              height: 70,
-              width: 10,
-              decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(5),
-                      bottomRight: Radius.circular(5))),
-            )
-          ],
+      child: InkWell(
+        onTap: (){
+          myProvider.setEspai(espai);
+        },
+        child: Card(
+          color: getColor(),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.all(10),
+                height: 50,
+                width: 50,
+                decoration:
+                    BoxDecoration(color: Colors.green, shape: BoxShape.circle),
+                child: Icon(Icons.home),
+              ),
+              Text(espai.nom),
+              Container(
+                margin: EdgeInsets.only(left: 200),
+                height: 70,
+                width: 10,
+                decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(5),
+                        bottomRight: Radius.circular(5))),
+              )
+            ],
+          ),
         ),
       ),
     );
